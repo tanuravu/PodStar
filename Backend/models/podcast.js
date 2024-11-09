d8a8fceb9c5b1342637367ffc51229e414226769
TARANPREET <tanu04@gmail.com>
2024-11-09 09:53:26 +0530

    Podcast and category Schema is created in models folder

---
+++ Backend/models/podcast.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose');
+
+const podcasts = new mongoose.Schema({
+    
+    frontImage:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    audioFile:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    title:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    description:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    user:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"user",
+        },
+    category:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"category",
+        },
+},
+{timestamps:true}
+);
+
+module.exports= mongoose.model('user',user);

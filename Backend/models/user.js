f4502c440a7c92521918abddf84c8b11e7f0ed0a
TARANPREET <tanu04@gmail.com>
2024-11-09 09:50:32 +0530

    User Schema is created in models folder

---
+++ Backend/models/user.js
@@ -0,0 +1,37 @@
+const mongoose=require('mongoose');
+
+const user = new mongoose.Schema({
+    
+    username:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    password:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    /* one user will create multiple podcasts */
+    podcasts:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"podcasts",
+        },
+    ],
+ /*    favourites:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"podcasts",
+        },
+    ], */
+},
+{timestamps:true}
+);
+
+module.exports= mongoose.model('user',user);

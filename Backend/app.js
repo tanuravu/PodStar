48cb21c6739199851f97a4f52f3857b86f389817
TARANPREET <tanu04@gmail.com>
2024-11-11 20:33:25 +0530

    category.js added in routes and modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const cookieParser=require("cookie-parser");
 const userApi = require("./routes/user"); 
+const catApi = require("./routes/category"); 
 require("dotenv").config();
 require("./connection/connection");
 app.use(cookieParser());
 app.use(express.json());
+
+
+
+
+
+
 // all routes
 app.use("/api/v1", userApi);
+app.use("/api/v1", catApi);
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`);
--- Backend/models/category.js
+++ Backend/models/category.js
@@ -17,4 +17,4 @@ const category = new mongoose.Schema({
 {timestamps:true}
 );
 
-module.exports= mongoose.model('user',user);
+module.exports= mongoose.model('category',category);

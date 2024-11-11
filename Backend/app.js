82edd43f8faa0d60a812110e3b3fcd53b81cb3df
TARANPREET <tanu04@gmail.com>
2024-11-11 14:56:30 +0530

    user.js modified in router folder,backend of user created.

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const cookieParser=require("cookie-parser");
 const userApi = require("./routes/user"); 
 require("dotenv").config();
 require("./connection/connection");
-
-// all routes
+app.use(cookieParser());
 app.use(express.json());
+// all routes
 app.use("/api/v1", userApi);
 
 app.listen(process.env.PORT, () => {
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,7 @@
 const router=require("express").Router();
 const User= require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt=require("jsonwebtoken");
 
 //signup-route
 router.post("/sign-up",async (req,res) =>{
@@ -32,14 +33,67 @@ router.post("/sign-up",async (req,res) =>{
         }
         catch(error){
         console.log(error);
-        res.status(400).json({error});
+        res.status(500).json({error});
     }
 });
 
 //signin-route
+router.post("/sign-in",async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+        if(!email||!password){
+            return res.status(400).json({message:"All fields  are required"});
+        }
+        //to check user exist or not 
+        const existingUser= await User.findOne({email:email});
+        if(!existingUser){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        //to check password is same or not
+        const isMatch=await bcrypt.compare(password,existingUser.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        //Generate JWT token
+        const token=jwt.sign(
+        {id:existingUser._id,email:existingUser.email},
+        process.env.JWT_SECRET,
+        {expiresIn:"30d" }
+    );
+    res.cookie("podcasterUSerToken",token,{
+        httpOnly: true,
+        maxAge: 30*24*60*60*1000,//30days
+        secure: process.env.NODE_ENV=== "production",
+        sameSite: "None", 
+    });
+    return res.status(200).json({
+        id:existingUser._id,
+        username:existingUser.username,
+        email:email,
+        message:"Sign-in Successfully",
+    })
+    }catch(error){
+        res.status(500).json({error});
+    }
+});
+
+//LOGOUT ROUTER:
+router.post("/logout",async(req,res)=>{
+    res.clearCookie("podcasterUSerToken",{
+        httpOnly: true,
+    });
+    res.json({message: "Logged out"});
+});
 
 
-
-
-
+// TO CHECK COOKIE IS PRESENT OR NOT
+router.get("/check-cookie", async (req, res) => {
+    const token = req.cookies.podcasterUSerToken;
+    if (token) {
+        return res.status(200).json({ message: "true" });
+    }
+    return res.status(200).json({ message: "false" });
+});
 module.exports=router;

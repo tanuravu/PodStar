8ea8730d83df9eca9223cb568da7a5287ff7cbf1
TARANPREET <tanu04@gmail.com>
2024-11-21 10:35:28 +0530

    Created Login and Signup page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import MainLayout from './layout/MainLayout';
-import Home from './pages/home';
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import AuthLayout from "./layout/AuthLayout";
+import Login from "./pages/Login.jsx";
+
 const App = () => {
   return (
-    <div>
+    <div className="">
       <Router>
-        <Routes>
+        <Routes >
           <Route path="/" element={<MainLayout/>}>
-          <Route index element = {<Home/>}/></Route>
-         
+          {" "}
+          <Route index element={<Home/>} />
+            {" "}
+            <Route index element={<Home/>} />
+          </Route>
+          <Route path="/" element={<AuthLayout/>}>
+            <Route path="/signup" element={<Signup/>} />
+            <Route path="/login" element={<Login/>} />
+          </Route> 
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
--- Frontend/src/layout/AuthLayout.jsx
+++ Frontend/src/layout/AuthLayout.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import {Outlet} from "react-router-dom";
 
 const AuthLayout = () => {
-    return (
-        <div>
-            AuthLayout
-        </div>
-    );
-}
+  return (
+    <div>
+      <Outlet/>
+    </div>
+    
+  );
+};
 
 export default AuthLayout;
